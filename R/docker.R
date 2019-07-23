d05adf24a68a05bb0a8a4be13c4dddce5b8d6a42
Ruan Pearce-Authers <ruan@hypothesci.com>
2019-07-23 23:01:03 +0100

    Use temp dir for Docker builds, testing Packrat support

--- R/docker.R
+++ R/docker.R
@@ -13,15 +13,45 @@ docker_deploy <- function(image) {
 	base <- paste0("r-base:", version)
 	apt_deps <- c("libcurl4-openssl-dev", "libxml2-dev", "libssl-dev")
 
+	rscript <- function(code) {
+		paste0("RUN Rscript -e \"", code, "\"")
+	}
+
+	context_dir <- tempdir()
+
+	using_packrat <- requireNamespace("packrat", quietly = T) && any(grepl("packrat", packrat::search_path()$lib.dir))
+
+	if (using_packrat) {
+		target_packrat_dir <- file.path(context_dir, "packrat")
+		src_packrat_dir <- file.path(getwd(), "packrat")
+
+		packrat_files <- sapply(c("packrat.lock", "init.R"), function(f) file.path(src_packrat_dir, f))
+
+		dir.create(target_packrat_dir)
+		file.copy(packrat_files, target_packrat_dir)
+
+		bootstrap_operations <- c(
+			"install.packages('packrat')",
+			"packrat::restore()",
+			"packrat::packify()"
+		)
+	} else {
+		bootstrap_operations <- c(
+			"install.packages('remotes')",
+			"remotes::install_github('hypothesci/cloudburst')"
+		)
+	}
+
 	lines <- c(
 		paste0("FROM ", base),
 		paste0("RUN apt-get update && apt-get install -y ", paste(apt_deps, collapse = " ")),
-		"RUN Rscript -e \"install.packages('remotes'); remotes::install_github('hypothesci/cloudburst')\"",
+		rscript(paste(bootstrap_operations, collapse = "; ")),
 		"CMD [ \"Rscript\", \"-e\", '\"cloudburst::runner()\""
 	)
 
-	dockerfile <- paste(lines, collapse = "\n")
-	build_status <- system(paste0("docker build -t ", image,  " -"), input = dockerfile)
+	writeLines(lines, con = file.path(context_dir, "Dockerfile"))
+
+	build_status <- system(paste0("docker build -t ", image,  " ", context_dir))
 	if (build_status != 0) stop("docker build failed")
 
 	push_status <- system(paste0("docker push ", image))
