524b759c7d20e1d6581dc82776051fc57d2f7406
Ruan Pearce-Authers <ruan@hypothesci.com>
2019-07-25 23:12:05 +0100

    Fix runner function invocation

--- R/runner.R
+++ R/runner.R
@@ -10,6 +10,6 @@ runner <- function() {
 	args <- lapply(bootstrap$dependency_ids, function(i) storage_read(bootstrap$storage, c(bootstrap$stage_storage_prefix, i, "result.rds")))
 	stage_data <- storage_read(bootstrap$storage, c(bootstrap$stage_storage_prefix, bootstrap$stage_id, "code.rds"))
 	attach(stage_data$globals)
-	res <- do.call(fn, args)
+	res <- do.call(stage_data$fn, args)
 	storage_write(bootstrap$storage, c(bootstrap$stage_storage_prefix, bootstrap$stage_id, "result.rds"), res)
 }
